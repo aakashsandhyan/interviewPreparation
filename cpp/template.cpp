09f4142662ab58167b104829d378d15ac3a207f1
Aakash Sandhyan <aakashsandhyan@gmail.com>
2020-11-03 16:48:32 +0530

    Adding template for c++

---
+++ cpp/template.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+void solve(){
+	cout<<"Solution starts here";
+}
+int main(){
+	//to do
+	int t,n;
+	scanf("%i", &t);
+	while(t--){
+		solve();
+	}
+	return 0;
+}
